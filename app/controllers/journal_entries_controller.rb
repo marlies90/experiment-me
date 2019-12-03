90102f0d34071a74f0f104830ec667c8e80e8f93
Marlies Gielen <m.gielen@youngcapital.nl>
2019-12-03 12:24:26 +0100

    Add journal_entry controller and views

---
+++ app/controllers/journal_entries_controller.rb
@@ -0,0 +1,57 @@
+class JournalEntriesController < ApplicationController
+  before_action :set_user
+  before_action :set_journal_entry, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @journal_entry = JournalEntry.new
+  end
+
+  def edit
+  end
+
+  def create
+    @journal_entry = JournalEntry.new(journal_entry_params)
+    @journal_entry.user_id = current_user.id
+    binding.pry
+
+    if @journal_entry.save
+      redirect_to dashboard_journal_path, notice: 'Journal entry was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @journal_entry.user_id = current_user.id
+    if @journal_entry.update(journal_entry_params)
+      redirect_to dashboard_journal_path, notice: 'Journal entry was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+    authorize self
+  end
+
+  def set_journal_entry
+    @journal_entry = JournalEntry.find(params[:id])
+  end
+
+  def journal_entry_params
+    params.fetch(:journal_entry).permit(
+      :date, :user_id,
+      journal_ratings_attributes: [:journal_statement_id, :score]
+    )
+  end
+end
--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,4 +1,8 @@
 class JournalEntry < ApplicationRecord
   belongs_to :user
   has_many :journal_ratings
+
+  validates_presence_of :date, :user_id
+
+  accepts_nested_attributes_for :journal_ratings
 end
