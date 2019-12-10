832e4fc16d0d7cbf51987303f1d9797b04bb8e6d
Marlies Gielen <m.gielen@youngcapital.nl>
2019-12-10 17:00:26 +0100

    Add validations and dashboard page for journal_entries

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -3,6 +3,10 @@ class JournalEntry < ApplicationRecord
   has_many :journal_ratings
 
   validates_presence_of :date, :user_id
+  validates_uniqueness_of :date, scope: :user_id
 
   accepts_nested_attributes_for :journal_ratings
+  validates_associated :journal_ratings
+
+  scope :newest, -> { order("date DESC") }
 end
--- app/models/journal_rating.rb
+++ app/models/journal_rating.rb
@@ -1,4 +1,6 @@
 class JournalRating < ApplicationRecord
   belongs_to :journal_entry
   has_one :journal_statement
+
+  validates_presence_of :journal_statement_id, :score
 end
