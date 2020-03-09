ceee606224b58f4e9dc4ede13daad85d59344805
Marlies Gielen <m.gielen@youngcapital.nl>
2020-03-09 09:42:14 +0100

    Remove validate:false as it turns out not to be needed

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -7,7 +7,7 @@ class Experiment < ApplicationRecord
   has_many :experiment_users
   has_many :users, through: :experiment_users
   has_many :experiment_benefits
-  has_many :benefits, through: :experiment_benefits, validate: false
+  has_many :benefits, through: :experiment_benefits
   has_one_attached :image
 
   accepts_nested_attributes_for :resources, reject_if: lambda { |attrs| attrs['name'].blank? }
