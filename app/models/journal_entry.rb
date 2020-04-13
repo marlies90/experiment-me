4ebbf603ed13c667607b3952a1b52c1ec3921a76
Marlies Gielen <m.gielen@youngcapital.nl>
2020-04-13 13:47:30 +0200

    Styling update for experiment_users form

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -10,8 +10,11 @@ class JournalEntry < ApplicationRecord
 
   validates_presence_of :date, :user_id, :journal_ratings
   validates :experiment_success,
-    inclusion: { in: [true, false], message: "Fill in whether you stuck to the experiment" },
-    if: -> { active_experiment }
+            inclusion: {
+              in: [true, false],
+              message: "Fill in whether you stuck to the experiment"
+            },
+            if: -> { active_experiment }
   validates_uniqueness_of :date, scope: :user_id
 
   accepts_nested_attributes_for :journal_ratings
