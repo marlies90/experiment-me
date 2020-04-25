ea7a9d232d9b0756fe5cffb32b68035903a250c5
Marlies Gielen <m.gielen@youngcapital.nl>
2020-04-25 21:14:47 +0200

    Fix merge conflict leftovers

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,19 +3,11 @@ class Rack::Attack
     req.ip
   end
 
-<<<<<<< HEAD
-  Rack::Attack.throttle("login_ip", limit: 5, period: 5.min) { |req|
-    req.ip if req.post? && req.path == "/users/sign_in"
-  }
-
-  Rack::Attack.throttle("login_email", limit: 5, period: 5.min) { |req|
-=======
   Rack::Attack.throttle("login_ip", limit: 5, period: 5.minutes) { |req|
     req.ip if req.post? && req.path == "/users/sign_in"
   }
 
   Rack::Attack.throttle("login_email", limit: 5, period: 5.minutes) { |req|
->>>>>>> security-upgrades
     req.params["email"].presence if req.post? && req.path == "/users/sign_in"
   }
 end
