dce9d72c61739d9a085b903885ab6c297bfbbb5d
Marlies Gielen <m.gielen@youngcapital.nl>
2020-04-26 09:54:35 +0200

    Remove error message for now, will add later

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,24 +5,9 @@ class Rack::Attack
 
   Rack::Attack.throttle("login_ip", limit: 7, period: 5.minutes) { |req|
     req.ip if req.post? && req.path == "/users/sign_in"
-    too_many_login_tries_message(req)
   }
 
   Rack::Attack.throttle("login_email", limit: 7, period: 5.minutes) { |req|
     req.params["email"].presence if req.post? && req.path == "/users/sign_in"
-    too_many_login_tries_message(req)
   }
-
-  private
-
-  def too_many_login_tries_message(req)
-    Rack::Attack.throttled_callback = lambda do |_env|
-      [
-        403,
-        { "Content-Type" => "text/plain" },
-        [ "You've tried to log in too many times. \
-          Your account has been locked for 5 minutes, after which you can try again." ]
-      ]
-    end
-  end
 end
