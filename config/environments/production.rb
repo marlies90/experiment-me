71bcdaa4555fc2f524869d46ad1bafe381e5afcd
Marlies Gielen <m.gielen@youngcapital.nl>
2020-05-15 10:55:39 +0200

    Fix rubocop errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,17 +68,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => "https://experiment-rocks.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://experiment-rocks.herokuapp.com/" }
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :address        => "smtp.sendgrid.net",
-      :port           => "587",
-      :authentication => :plain,
-      :user_name      => ENV["SENDGRID_USERNAME"],
-      :password       => ENV["SENDGRID_PASSWORD"],
-      :domain         => "heroku.com",
-      :enable_starttls_auto => true
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: plain,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
