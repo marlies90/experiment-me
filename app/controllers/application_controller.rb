ef86f47ba4f1cc74121fd6d8204ada7357f75f3e
Marlies Gielen <m.gielen@youngcapital.nl>
2020-06-15 11:02:25 +0200

    Set user timezone in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,17 @@ class ApplicationController < ActionController::Base
 
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :set_time_zone, if: :current_user
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name time_zone])
+  end
+
+  private
+
+  def set_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
   end
 end
