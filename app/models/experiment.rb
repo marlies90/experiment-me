1f28da9ce224f4156d8c9e3b8c069770f170b686
Marlies Gielen <m.gielen@youngcapital.nl>
2020-07-26 18:21:39 +0200

    Fix removing resources from an experiment, minor styling changes to experiment_show and category_show

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -5,14 +5,16 @@ class Experiment < ApplicationRecord
   friendly_id :name, use: :slugged
 
   belongs_to :category
-  has_many :resources
+  has_many :resources, dependent: :destroy
   has_many :experiment_users
   has_many :users, through: :experiment_users
   has_many :experiment_benefits
   has_many :benefits, through: :experiment_benefits
   has_one_attached :image
 
-  accepts_nested_attributes_for :resources, reject_if: ->(attrs) { attrs["name"].blank? }
+  accepts_nested_attributes_for :resources,
+                                allow_destroy: true,
+                                reject_if: ->(attrs) { attrs["name"].blank? }
 
   validates_presence_of :name, :description, :category, :objective, :benefits
 end
