6b4ab3af2356b1a46f5864c8fe6d4ebc50687e8d
Marlies Gielen <m.gielen@youngcapital.nl>
2020-09-24 10:52:58 +0200

    Add rescue from SMTP errors

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  rescue_from Net::SMTPAuthenticationError do
+    redirect_back(fallback_location: home_path, alert: "Error: email could not be sent")
+  end
+
   default from: "info@experiment.rocks"
   layout "mailer"
 end
