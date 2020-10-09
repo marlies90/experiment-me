ce5d908329014513cb807407f3f3101169c360ea
Marlies Gielen <m.gielen@youngcapital.nl>
2020-10-09 11:21:11 +0200

    Move  date formatting from helper to config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def to_journal_date(date)
-    date.strftime("%e %b %C%y (%a)")
-  end
-
-  def to_journal_date_no_year(date)
-    date.strftime("%e %b (%a)")
-  end
 end
