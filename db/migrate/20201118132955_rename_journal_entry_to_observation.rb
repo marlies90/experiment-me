3d02c914a1f42c05b62b76036562c0d2193d3a41
Marlies Gielen <m.gielen@youngcapital.nl>
2020-12-10 10:14:21 +0100

    Fix failing migration

--- db/migrate/20201118132955_rename_journal_entry_to_observation.rb
+++ db/migrate/20201118132955_rename_journal_entry_to_observation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class RenameObservationToObservation < ActiveRecord::Migration[6.0]
+class RenameJournalEntryToObservation < ActiveRecord::Migration[6.0]
   def change
-    rename_table :observations, :observations
+    rename_table :journal_entries, :observations
   end
 end
