2583cacad6512bf007ac0a26374411cfce54c3d1
Marlies Gielen <m.gielen@youngcapital.nl>
2021-01-29 11:01:39 +0100

    Replace testjob with experiment_midway_mail_job + tests

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -5,4 +5,8 @@ class UserMailerPreview < ActionMailer::Preview
   def experiment_start_email
     UserMailer.with(user: User.first, experiment: Experiment.first).experiment_start_email
   end
+
+  def experiment_midway_email
+    UserMailer.with(user: User.first, experiment: Experiment.first).experiment_midway_email
+  end
 end
