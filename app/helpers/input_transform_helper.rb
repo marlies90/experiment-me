f07aff174be718d00e9ff90905b1106e76c316bf
Marlies Gielen <m.gielen@youngcapital.nl>
2021-04-15 15:36:31 +0200

    Add images to blogs + header styling experiments

--- app/helpers/input_transform_helper.rb
+++ app/helpers/input_transform_helper.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 module InputTransformHelper
-  def add_header_css_classes_blog(description)
+  def transform_blog_input(description)
+    description_with_header_classes = add_css_classes_blog(description)
+    description_with_all_css_classes = transform_figcaption_tag(description_with_header_classes)
+    create_image_tags(description_with_all_css_classes)
+  end
+
+  def add_css_classes_blog(description)
     headers = {
       "<h2>" => "<h2 class='text-intense h3'>",
       "<h3>" => "<h3 class='h4'>",
@@ -12,11 +18,30 @@ module InputTransformHelper
     description.gsub(/<h\d>/, headers)
   end
 
+  def transform_figcaption_tag(description)
+    description.gsub("<figcaption>", "<figcaption class='figure-caption text-center'>")
+  end
+
   def add_header_css_classes_experiment(description)
     headers = {
-      "<h3>" => "<h3 class='text-intense h6'>"
+      "<h3>" => "<h3 class='text-intense h5'>",
+      "<h4>" => "<h4 class='text-intense h6'>"
     }
 
     description.gsub(/<h\d>/, headers)
+  end
+
+  def create_image_tags(description)
+    matches = description.scan(/image:\w*/)
+
+    matches.to_a.each do |match|
+      image_name = match.remove("image:")
+      image_tag = image_with_alt_tag(image_name)
+      next unless image_tag
+
+      description = description.sub!(match, image_tag)
+    end
+
+    description
   end
 end
--- spec/helpers/input_transform_helper_spec.rb
+++ spec/helpers/input_transform_helper_spec.rb
@@ -3,12 +3,18 @@
 require "rails_helper"
 
 RSpec.describe InputTransformHelper, type: :helper do
-  describe "#add_header_css_classes_blog" do
-    blog_post_description = "<h2>hi</h2> some text <h3>a header</h3>"
-
-    it "returns amount of days since start of the experiment and today" do
-      expect(helper.add_header_css_classes_blog(blog_post_description)).to eq(
+  describe "#transform_blog_input" do
+    it "returns the transformed headers" do
+      blog_post_description = "<h2>hi</h2> some text <h3>a header</h3>"
+      expect(helper.transform_blog_input(blog_post_description)).to eq(
         "<h2 class='text-intense h3'>hi</h2> some text <h3 class='h4'>a header</h3>"
+      )
+    end
+
+    it "returns the transformed figcaption" do
+      blog_post_description = "<figcaption>This is nice</figcaption>"
+      expect(helper.transform_blog_input(blog_post_description)).to eq(
+        "<figcaption class='figure-caption text-center'>This is nice</figcaption>"
       )
     end
   end
